bab372cfa10481a7aca9c1713a2f232403e5835c
mimea005 <30933848+Mimea005@users.noreply.github.com>
2025-02-28 17:12:06 +0000

    Added lsp keymaps

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,14 +1,8 @@
+---@type LazySpec[]
 return {
 	{
 		'neovim/nvim-lspconfig',
-		dependencies = {
-			{
-				'williamboman/mason-lspconfig.nvim',
-				dependencies = {
-					{ 'williamboman/mason.nvim' },
-				}
-			},
-		},
+		event = "VeryLazy",
 		config = function(_, opts)
 			require("mason").setup()
 			require("mason-lspconfig").setup()
@@ -23,17 +17,37 @@ return {
 					require("lspconfig")[server_name].setup {config}
         end,
 			}
-		end
+		end,
+		keys = {
+			{'<leader>lr', function() vim.lsp.buf.rename() end, desc="[r]ename"},
+			{'<leader>la', function() vim.lsp.buf.code_action() end, desc="[a]ction"},
+			{'<leader>lf', function() vim.lsp.buf.format() end, desc="[f]ormat"},
+			{'<leader>ld', function() vim.lsp.buf.definition() end, desc="[d]efinition"},
+			{'<leader>lD', function() vim.lsp.buf.declaration() end, desc="[d]eclaration"},
+			{'<leader>lt', function() vim.lsp.buf.type_definition() end, desc="[t]ype"},
+			{'<leader>li', function() vim.lsp.buf.implementation() end, desc="[i]mplementation"},
+			{'<leader>lc', function() vim.lsp.buf.incoming_calls() end, desc="[c]alled from"},
+			{'<leader>lC', function() vim.lsp.buf.outgoing_calls() end, desc="[C]alls"},
+		},
+		dependencies = {
+			'saghen/blink.cmp',
+			{
+				"folke/lazydev.nvim",
+				ft = "lua", -- only load on lua files
+				opts = {
+					library = {
+						-- See the configuration section for more details
+						-- Load luvit types when the `vim.uv` word is found
+						{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+					},
+				},
+			},
+			{
+				'williamboman/mason-lspconfig.nvim',
+				dependencies = {
+					{ 'williamboman/mason.nvim' },
+				}
+			},
+		},
 	},
-	{
-    "folke/lazydev.nvim",
-    ft = "lua", -- only load on lua files
-    opts = {
-      library = {
-        -- See the configuration section for more details
-        -- Load luvit types when the `vim.uv` word is found
-        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-      },
-    },
-  },
 }
