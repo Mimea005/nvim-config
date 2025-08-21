6b7c078a4c50900d4d99eec90cdf9c9402065227
mimea005 <30933848+Mimea005@users.noreply.github.com>
2025-08-21 14:56:36 +0000

    Add changes in config file for lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -8,34 +8,33 @@ return {
 			require("mason").setup()
 			require("mason-lspconfig").setup()
 
-			local set_capabilites = function (capabilities)
+			local set_capabilites = function(capabilities)
 				return require('blink.cmp').get_lsp_capabilities(capabilities)
 			end
 
 			require("mason-lspconfig").setup_handlers {
-        -- The first entry (without a key) will be the default handler
-        -- and will be called for each installed server that doesn't have
-        -- a dedicated handler.
-        function (server_name) -- default handler (optional)
+				-- The first entry (without a key) will be the default handler
+				-- and will be called for each installed server that doesn't have
+				-- a dedicated handler.
+				function(server_name) -- default handler (optional)
 					local config = opts[server_name] or {}
 					config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
-					lspconfig[server_name].setup {config}
-        end,
+					lspconfig[server_name].setup { config }
+				end,
 			}
 
 			lspconfig.gopls.setup(set_capabilites(opts["gopls"] or {}))
-
 		end,
 		keys = {
-			{'<leader>lr', function() vim.lsp.buf.rename() end, desc="[r]ename"},
-			{'<leader>la', function() vim.lsp.buf.code_action() end, desc="[a]ction"},
-			{'<leader>lf', function() vim.lsp.buf.format() end, desc="[f]ormat"},
-			{'<leader>ld', function() vim.lsp.buf.definition() end, desc="[d]efinition"},
-			{'<leader>lD', function() vim.lsp.buf.declaration() end, desc="[d]eclaration"},
-			{'<leader>lt', function() vim.lsp.buf.type_definition() end, desc="[t]ype"},
-			{'<leader>li', function() vim.lsp.buf.implementation() end, desc="[i]mplementation"},
-			{'<leader>lc', function() vim.lsp.buf.incoming_calls() end, desc="[c]alled from"},
-			{'<leader>lC', function() vim.lsp.buf.outgoing_calls() end, desc="[C]alls"},
+			{ '<leader>lr', function() vim.lsp.buf.rename() end,          desc = "[r]ename" },
+			{ '<leader>la', function() vim.lsp.buf.code_action() end,     desc = "[a]ction" },
+			{ '<leader>lf', function() vim.lsp.buf.format() end,          desc = "[f]ormat" },
+			{ '<leader>ld', function() vim.lsp.buf.definition() end,      desc = "[d]efinition" },
+			{ '<leader>lD', function() vim.lsp.buf.declaration() end,     desc = "[d]eclaration" },
+			{ '<leader>lt', function() vim.lsp.buf.type_definition() end, desc = "[t]ype" },
+			{ '<leader>li', function() vim.lsp.buf.implementation() end,  desc = "[i]mplementation" },
+			{ '<leader>lc', function() vim.lsp.buf.incoming_calls() end,  desc = "[c]alled from" },
+			{ '<leader>lC', function() vim.lsp.buf.outgoing_calls() end,  desc = "[C]alls" },
 		},
 		dependencies = {
 			'saghen/blink.cmp',
@@ -58,4 +57,8 @@ return {
 			},
 		},
 	},
+	{
+		"pcolladosoto/tinygo.nvim",
+		config = function() require("tinygo").setup() end
+	}
 }
